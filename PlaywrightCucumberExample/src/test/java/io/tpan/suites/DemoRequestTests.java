5933719c84e61cfd3de5007a10594f5c1022a94f
Terence Pan's Github for Code Samples <43430672+terencenmnpan@users.noreply.github.com>
2022-10-01 23:07:32 -0700

    Pom and suite updates

---
+++ PlaywrightCucumberExample/src/test/java/io/tpan/suites/DemoRequestTests.java
@@ -0,0 +1,13 @@
+package io.tpan.suites;
+
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@SuiteDisplayName("Test Use Cases")
+@IncludeEngines("cucumber")
+@SelectClasspathResource("Features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+public class DemoRequestTests {
+}
